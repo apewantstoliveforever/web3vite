4c04fa109d7312bb50efe42b5f46ccea6fdbf2de
LyHoaiLam <hoailam03999@gmail.com>
2024-07-30 21:41:29 +0700

    push code

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/globals.css'
 import { 
   UploadOutlined,
   UserOutlined,
@@ -6,23 +7,51 @@ import {
 import { Layout, Menu, theme, Image } from 'antd';
 
 
-const { Header, Content, Footer, Sider } = Layout;
+const { Content, Sider } = Layout;
+
+// const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
+//   (icon, index) => ({
+//     key: String(index + 1),
+//     icon:  <div className="flex items-center">
+
+//       <a href={`/chat${index + 1}`}>
+//         <Image
+//           width={50}
+//           className="rounded-full"
+//           src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+//         />
+//       </a>
+//   </div>,
+//     label: `Groups ${index + 1}`,
+//   }),
+// );
+
 
 const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
   (icon, index) => ({
     key: String(index + 1),
-    icon:  <div className="flex items-center">
-
-      <a href={`/chat${index + 1}`}>
-        <Image
-          width={50}
-          className="rounded-full"
-          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-        />
-      </a>
-  </div>,
+    icon: (
+      <div className="flex items-center">
+        {index >= 1 ? (
+          <a href={`/test`}>
+            <Image
+              width={50}
+              className="rounded-full"
+              src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            />
+          </a>
+        ) : (
+          <Image
+            width={50}
+            className="rounded-full"
+            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          />
+        )}
+      </div>
+    ),
     label: `Groups ${index + 1}`,
-  }),
+    
+  })
 );
 
 
@@ -33,74 +62,58 @@ const Home: React.FC = () => {
   } = theme.useToken();
 
   return (
-    <Layout>
-    <Sider
-      breakpoint="lg"
-      collapsedWidth="0"
-      onBreakpoint={(broken) => {
-        console.log(broken);
-      }}
-      onCollapse={(collapsed, type) => {
-        console.log(collapsed, type);
-      }}
-    >
-      <div className="demo-logo-vertical" />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
-    </Sider>
 
 
-     <Content style={{ margin: '24px 16px 0' }}>
-
-        <div
-            className='class="bg-red-500 hover:bg-red-700'
-            style={{
-              padding: 24,
-              display: 'flex',
-              minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-              
-            }}
-          >
-
-            <div className='items-center'>
-              <Image
-                width={50}
-                className="rounded-full"
-                src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-              />
-            </div>
-
-            <div className='text-left ml-5 bg-gray-200 h-full'>
-                <h1 className='hover:text-red-500'>User: Hoài Lâm</h1>
-                <h4>Thành viên đóng góp đáng kể</h4>
-                <p>
-                  Hồ Chí Minh: Phòng 12, Nhà 8, Đường số 13, Công viên phần mềm Quang Trung, P. Tân Chánh Hiệp, Q.12, TP.HCM, Quận 12 
-                </p>
-            </div>
-          </div>
-      </Content>
+    <>
+      <Layout className="bg-neutral-900 px-28 h-lvh">
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+      </Sider>
 
 
-    {/* <Layout>
-      <Header style={{ padding: 0, background: colorBgContainer }} />
-      // <Content style={{ margin: '24px 16px 0' }}>
-      //   <div
-      //     style={{
-      //       padding: 24,
-      //       minHeight: 360,
-      //       background: colorBgContainer,
-      //       borderRadius: borderRadiusLG,
-      //     }}
-      //   >
-      //     content
-      //   </div>
-      // </Content>
-      <Footer style={{ textAlign: 'center' }}>
-        Ant Design ©{new Date().getFullYear()} Created by Ant UED
-      </Footer>
-    </Layout> */}
-  </Layout>
+      <Content className='bg-slate-800'>
+
+          {/* <div
+              className='bg-red-500 hover:bg-red-700'
+              style={{
+                padding: 24,
+                display: 'flex',
+                minHeight: 360,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+                
+              }}
+            >
+
+              <div className='items-center'>
+                <Image
+                  width={50}
+                  className="rounded-full"
+                  src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                />
+              </div>
+
+              <div className='text-left ml-5 bg-gray-200 h-full'>
+                  <h1 className='hover:text-red-500'>User: Hoài Lâm</h1>
+                  <h4>Thành viên đóng góp đáng kể</h4>
+                  <p>
+                    Hồ Chí Minh: Phòng 12, Nhà 8, Đường số 13, Công viên phần mềm Quang Trung, P. Tân Chánh Hiệp, Q.12, TP.HCM, Quận 12 
+                  </p>
+              </div>
+            </div> */}
+        </Content>
+    </Layout>
+    </>
   );
 };
 
