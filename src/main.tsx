fdefbf5d3136cad17fd6fc7ae767b2e017ebcf67
Jake Hu <hotranphihung@gmail.com>
2024-07-30 19:45:26 +0700

    s

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import * as process from 'process';
+(window as any).global = window;
+(window as any).process = process;
+(window as any).Buffer = [];
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
